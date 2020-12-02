a287526f25d5ffce6512608f86c70fff0aafd67c
FachryRizano <fachry.rizano@gmail.com>
2020-12-02 18:03:52 +0700

    menambahkan schedule record

--- env/main.py
+++ env/main.py
@@ -1,15 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.wait import WebDriverWait    
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 import time
 import pyautogui as pyg
+import numpy as np
+import pandas as pd
 #Import selenium
 chrome_driver=r"C:\Users\Asus\Desktop\auto-login\chromedriver.exe"
 #chrome's webdriver location, this is an exe file
 driver = webdriver.Chrome(chrome_driver)
 
+container = []
+container2 = []
+i=0
 
 #Load driver
 driver.maximize_window()
@@ -28,11 +33,16 @@ submit = driver.find_element_by_id("btnSubmit")
 submit.click()
 
 time.sleep(2)
-date = driver.find_element_by_xpath('//*[@id="studentViconList"]/tbody/tr[3]/td[1]')
-date2= driver.find_element_by_xpath('//*[@id="studentViconList"]/tbody/tr[4]/td[1]')
-print(date.text)
-print(date2.text)
-joinButton = driver.find_element_by_xpath('//*[@id="studentViconList"]/tbody/tr[20]/td[12]/div/a')
+table = WebDriverWait(driver, 8).until(lambda driver: driver.find_elements_by_css_selector("#studentViconList tr td"))
+for tr in table:
+    if(tr.text!=''):
+        container.append(tr.text)
+rows = len(driver.find_elements_by_xpath('//*[@id="studentViconList"]/tbody/tr'))
+header = driver.find_element_by_xpath('//*[@id="studentViconList"]/thead/tr')
+matrix = np.array(container).reshape((rows-2),12)
+df = pd.DataFrame(data=matrix,columns=['DATE','TIME','CLASS','ROOM','CAMPUS','DELIVERY MODE','COURSE','WEEK','SESSION','MEETING ID','MEETING PASSWORD','VICON'])
+df.to_csv(r'C:\Users\Asus\Desktop\auto-login\schedule_record.csv')
+joinButton = driver.find_element_by_xpath('//*[@id="studentViconList"]/tbody/tr[6]/td[12]/div/a')
 joinButton.click()
 
 print("Parent window title: " + driver.title)
